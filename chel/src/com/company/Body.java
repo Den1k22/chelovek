1d94f348c6404ad480ec82671541521c6ca40aa0
Den1k22 <deonisy@inbox.lv>
2014-02-01 12:29:57 +0200

    HumanParts

---
+++ chel/src/com/company/Body.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: java
+ * Date: 14.1.2
+ * Time: 11:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class Body {
+    public static void main(String args[])
+    {
+
+    }
+}
---
+++ chel/src/com/company/rLeg.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: java
+ * Date: 14.1.2
+ * Time: 11:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class rLeg {
+}
